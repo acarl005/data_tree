1e8f0822abaedb2b206be3fd16b2f60bcf3976ec
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-26 19:18:28 -0700

    change to spaces

--- rb/bin_search_tree.rb
+++ rb/bin_search_tree.rb
@@ -3,100 +3,100 @@ require 'pry'
 
 class BinSearchTree
 
-	include Enumerable
-	attr_reader :head
+  include Enumerable
+  attr_reader :head
 
 
-	def initialize(head=nil)
-		@head = head
-	end
+  def initialize(head=nil)
+    @head = head
+  end
 
-	def +(val)
-		node = Node.new(val)
-		if !@head
-			@head = node
-			return self
-		end
-		add_crawl(node)
-		self
-	end
+  def +(val)
+    node = Node.new(val)
+    if !@head
+      @head = node
+      return self
+    end
+    add_crawl(node)
+    self
+  end
 
-	def add_crawl(node, host=@head)
-		if node < host
-			if !host.left
-			  return host.left = node
-			else
-				host = host.left
-			end
-		else
-			if !host.right
-				return host.right = node
-			else
-				host = host.right
-			end
-		end
-		add_crawl(node, host)
-	end
+  def add_crawl(node, host=@head)
+    if node < host
+      if !host.left
+        return host.left = node
+      else
+        host = host.left
+      end
+    else
+      if !host.right
+        return host.right = node
+      else
+        host = host.right
+      end
+    end
+    add_crawl(node, host)
+  end
 
-	def each(&block)
-		return self if !@head
-		visit(@head, block)
-		self
-	end
+  def each(&block)
+    return self if !@head
+    visit(@head, block)
+    self
+  end
 
-	def visit(host, block)
-		return self if !host
-		visit(host.left, block)
-		block.call(host.val)
-		visit(host.right, block)
-	end
+  def visit(host, block)
+    return self if !host
+    visit(host.left, block)
+    block.call(host.val)
+    visit(host.right, block)
+  end
 
-	def height
-		return 0 if !@head
-		@head.height
-	end
+  def height
+    return 0 if !@head
+    @head.height
+  end
 
-	def right_height
-		return 0 if !@head.right
-		@head.right.height
-	end
+  def right_height
+    return 0 if !@head.right
+    @head.right.height
+  end
 
-	def left_height
-		return 0 if !@head.left
-		@head.left.height
-	end
+  def left_height
+    return 0 if !@head.left
+    @head.left.height
+  end
 
-	def display
-		repr(@head) if @head
-	end
+  def display
+    repr(@head) if @head
+  end
 
-	def repr(*hosts)
-		return '' if !hosts.any?
-		height = hosts.compact.map(&:height).max
-		buffer = indent(height)
-		str = ""
-		row = hosts.inject("") do |sum, node|
-			sum + buffer + node.val.to_s + buffer + " " rescue sum + buffer + " " + buffer + " "
-		end + " \n"
-		str << row
-		if height > 1
-			(2**(height-2)).times do
-				row = row.gsub(/ \//, '/ ').gsub(/\\ /, " \\").gsub(/ \d+ /, '/ \\')
-				str << row
-			end
-		end
-		return str + repr(*hosts.flat_map{ |host|
-			if !host
-				[nil, nil]
-			else
-				[host.left, host.right]
-			end
-		})
-	end
+  def repr(*hosts)
+    return '' if !hosts.any?
+    height = hosts.compact.map(&:height).max
+    buffer = indent(height)
+    str = ""
+    row = hosts.inject("") do |sum, node|
+      sum + buffer + node.val.to_s + buffer + " " rescue sum + buffer + " " + buffer + " "
+    end + " \n"
+    str << row
+    if height > 1
+      (2**(height-2)).times do
+        row = row.gsub(/ \//, '/ ').gsub(/\\ /, " \\").gsub(/ \d+ /, '/ \\')
+        str << row
+      end
+    end
+    return str + repr(*hosts.flat_map{ |host|
+      if !host
+        [nil, nil]
+      else
+        [host.left, host.right]
+      end
+    })
+  end
 
-	def indent(height)
-		" " * (1...height).inject { |sum, n| sum + 2 ** (n-1) } rescue ""
-	end
+  def indent(height)
+    " " * (1...height).inject { |sum, n| sum + 2 ** (n-1) } rescue ""
+  end
 
 end
 
--- rb/node.rb
+++ rb/node.rb
@@ -1,40 +1,40 @@
 class Node
 
-	attr_accessor :val, :left, :right
+  attr_accessor :val, :left, :right
 
-	def initialize(val)
-		@val = val
-		@left = nil
-		@right = nil
-	end
+  def initialize(val)
+    @val = val
+    @left = nil
+    @right = nil
+  end
 
-	def height(host=self)
-		return 0 if !host
-		1 + [height(host.left), height(host.right)].max
-	end
+  def height(host=self)
+    return 0 if !host
+    1 + [height(host.left), height(host.right)].max
+  end
 
-	def ==(node)
-		val == node
-	end
+  def ==(node)
+    val == node
+  end
 
-	def <=>(node)
-		val <=> node.val
-	end
+  def <=>(node)
+    val <=> node.val
+  end
 
-	def <(node)
-		val < node.val
-	end
+  def <(node)
+    val < node.val
+  end
 
-	def >(node)
-		val > node.val
-	end
+  def >(node)
+    val > node.val
+  end
 
-	def <=(node)
-		val <= node.val
-	end
+  def <=(node)
+    val <= node.val
+  end
 
-	def >=(node)
-		val >= node.val
-	end
+  def >=(node)
+    val >= node.val
+  end
 
 end
